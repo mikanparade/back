936789aa02bb01a5da5afd8f1f77f67b97eed390
Akira_0809 <102137735+Akira0809@users.noreply.github.com>
2023-12-15 10:06:29 +0900

    wip

--- server/calender/models.py
+++ server/calender/models.py
@@ -1,3 +1,44 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Calendar(models.Model):
+    name = models.CharField(max_length=255)
+    color = models.CharField(max_length=255)
+    user = models.ForeignKey(User, related_name='calendars', on_delete=models.CASCADE)
+
+    def to_updated(self, data: dict) -> 'Calendar':
+        if 'name' in data:
+            self.name = data['name']
+        if 'color' in data:
+            self.color = data['color']
+        return self
+
+class Event(models.Model):
+    summary = models.CharField(max_length=255)
+    description = models.TextField()
+    start = models.DateTimeField()
+    end = models.DateTimeField()
+    rrule = models.CharField(max_length=255, null=True, blank=True)
+    exclude_date = models.JSONField(default=list)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    recurrence_id = models.DateTimeField()
+
+    calendar = models.ForeignKey(Calendar, related_name='events', on_delete=models.CASCADE)
+
+    def to_updated(self, data: dict) -> 'Event':
+        if 'summary' in data:
+            self.name = data['summary']
+        if 'description' in data:
+            self.color = data['description']
+        if 'start' in data:
+            self.start = data['start']
+        if 'end' in data:
+            self.end = data['end']
+        if 'rrule' in data:
+            self.rrule = data['rrule']
+        if 'exclude_data' in data:
+            self.exclude_date = data['exclude_data']
+        if 'recurrence_id' in data:
+            self.recurrence_id = data['recurrence_id']
+        return self
--- server/calender/views.py
+++ server/calender/views.py
@@ -7,6 +7,7 @@ from django.utils.decorators import method_decorator
 from django.views.decorators.csrf import csrf_exempt
 from django.views.generic import View
 from django.contrib.auth.models import User
+from .models import Calendar, Event
 
 @method_decorator(csrf_exempt, name='dispatch')
 class CreateUserAPIView(View):
@@ -61,3 +62,51 @@ class UpdateUserAPIView(View):
         user.password = new_data.get('password', user.password)
         user.save()
         return JsonResponse({'message': 'User updated successfully'})
+
+@method_decorator(csrf_exempt, name='dispatch')
+class CalendarListView(View):
+    def get(self, request):
+        # リクエストしてきたユーザーが持っているカレンダーの一覧を取得
+        user_calendars = Calendar.objects.filter(user=request.user)
+        calendars_data = [{'id': calendar.id, 'name': calendar.name} for calendar in user_calendars]
+        return JsonResponse(calendars_data, safe=False)
+
+@method_decorator(csrf_exempt, name='dispatch')
+class EventListView(View):
+    def get(self, request, calendar_id):
+        try:
+            # カレンダーが存在するか確認
+            calendar = Calendar.objects.get(id=calendar_id, user=request.user)
+            # カレンダーに関連するイベントを取得
+            events_data = [{'id': event.id, 'summary': event.summary, 'start': event.start, 'end': event.end} for event in calendar.events.all()]
+            return JsonResponse(events_data, safe=False)
+        except Calendar.DoesNotExist:
+            return JsonResponse({'message': 'Calendar not found'}, status=404)
+
+    def post(self, request, calendar_id):
+        try:
+            # カレンダーが存在するか確認
+            calendar = Calendar.objects.get(id=calendar_id, user=request.user)
+            # リクエストデータを使ってイベントを作成
+            data = json.loads(request.body)
+            event = Event.objects.create(calendar=calendar, **data)
+            return JsonResponse({'id': event.id, 'message': 'Event created successfully'}, status=201)
+        except Calendar.DoesNotExist:
+            return JsonResponse({'message': 'Calendar not found'}, status=404)
+
+@method_decorator(csrf_exempt, name='dispatch')
+class EventDetailView(View):
+    def put(self, request, calendar_id, event_id):
+        try:
+            # カレンダーが存在するか確認
+            calendar = Calendar.objects.get(id=calendar_id, user=request.user)
+            # イベントが存在するか確認
+            event = Event.objects.get(id=event_id, calendar=calendar)
+            # リクエストデータを使ってイベントを更新
+            data = json.loads(request.body)
+            calendar.to_updated(data).save()
+            return JsonResponse({'message': 'Event updated successfully'}, status=204)
+        except Calendar.DoesNotExist:
+            return JsonResponse({'message': 'Calendar not found'}, status=404)
+        except Event.DoesNotExist:
+            return JsonResponse({'message': 'Event not found'}, status=404)
